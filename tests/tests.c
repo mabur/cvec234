262fedbef8c2303026af08b842a2579ee434ce9e
Magnus Burenius <magnus.burenius@gmail.com>
2025-09-18 08:56:05 +0200

    refactor: test mul_float2x2_float2

--- tests/tests.c
+++ tests/tests.c
@@ -26,7 +26,7 @@ void ASSERT_EQUAL_INT(const char* description, int a, int b) {
     }
 }
 
-void ASSERT_EQUAL_DOUBLE2(const char* description, double2 a, double2 b) {
+void ASSERT_EQUAL_FLOAT2(const char* description, float2 a, float2 b) {
     global_assert_count++;
     if (a[0] == b[0] && a[1] == b[1]) {
         printf("%s ok\n", description);
@@ -36,15 +36,15 @@ void ASSERT_EQUAL_DOUBLE2(const char* description, double2 a, double2 b) {
     }
 }
 
-#define ASSERT_NOT_EQUAL_INT(description, a, b) do { \
-    global_assert_count++; \
-    if (a != b) { \
-        printf("%s ok\n", (description)); \
-    } else { \
-        printf("%s %i==%i bad\n", (description), (a), (b)); \
-        global_assert_errors++; \
-    } \
-} while (0)
+void ASSERT_EQUAL_DOUBLE2(const char* description, double2 a, double2 b) {
+    global_assert_count++;
+    if (a[0] == b[0] && a[1] == b[1]) {
+        printf("%s ok\n", description);
+    } else {
+        printf("%s [%f,%f]!=[%f,%f] bad\n", description, a[0], a[1], b[0], b[1]);
+        global_assert_errors++;
+    }
+}
 
 void summarize_tests() {
     if (global_assert_errors != 0) {
@@ -65,6 +65,7 @@ int main() {
     ASSERT_EQUAL_INT("SQUARED_DISTANCE2", SQUARED_DISTANCE2(((int2){1,2}), ((int2){3,4})), 8);
     ASSERT_EQUAL_INT("SQUARED_DISTANCE3", SQUARED_DISTANCE3(((int3){1,2,3}), ((int3){4,5,6})), 27);
 
+    ASSERT_EQUAL_FLOAT2("mul_float2x2_float2", mul_float2x2_float2((float2x2){.columns={{1,2},{3,4}}}, (float2){1,2}), (float2){7,10});
     ASSERT_EQUAL_DOUBLE2("mul_double2x2_double2", mul_double2x2_double2((double2x2){.columns={{1,2},{3,4}}}, (double2){1,2}), (double2){7,10});
     summarize_tests();
     return 0;
