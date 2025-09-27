5273751b756a55ad1af1f19b7490a3b5315d99c8
Magnus Burenius <magnus.burenius@gmail.com>
2025-09-27 18:11:04 +0200

    refactor: extract matrix_core.h

--- cvec234/matrix.h
+++ cvec234/matrix.h
@@ -1,20 +1,13 @@
 #pragma once
 
 #include "vector.h"
+#include "matrix_core.h"
 
 #if defined(__GNUC__) || defined(__clang__)
     #define TYPE_OF_EXPRESSION(x) __typeof__(x)
 #elif defined(__cplusplus)
     #define TYPE_OF_EXPRESSION(x) decltype(x)
 #endif
-
-typedef struct float2x2 {float2 columns[2];} float2x2;
-typedef struct float3x3 {float3 columns[3];} float3x3;
-typedef struct float4x4 {float4 columns[4];} float4x4;
-
-typedef struct double2x2 {double2 columns[2];} double2x2;
-typedef struct double3x3 {double3 columns[3];} double3x3;
-typedef struct double4x4 {double4 columns[4];} double4x4;
 
 #define T_float2 TYPE_OF_EXPRESSION((float2){})
 #define T_float3 TYPE_OF_EXPRESSION((float3){})
---
+++ cvec234/matrix_core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "vector.h"
+
+typedef struct float2x2 {float2 columns[2];} float2x2;
+typedef struct float3x3 {float3 columns[3];} float3x3;
+typedef struct float4x4 {float4 columns[4];} float4x4;
+
+typedef struct double2x2 {double2 columns[2];} double2x2;
+typedef struct double3x3 {double3 columns[3];} double3x3;
+typedef struct double4x4 {double4 columns[4];} double4x4;
