b73c4254f702657db8109463867a883958985ebd
Magnus Burenius <magnus.burenius@gmail.com>
2025-11-09 16:56:53 +0100

    extend vector types up to 8 dimensions

--- cvec234/vector.h
+++ cvec234/vector.h
@@ -5,20 +5,36 @@
 #if defined(__clang__) || defined(__GNUC__)
 
 typedef unsigned char byte2 __attribute__((vector_size(2)));
-typedef unsigned char byte3 __attribute__((vector_size(4))); // Including padding
+typedef unsigned char byte3 __attribute__((vector_size(4)));  // Including padding
 typedef unsigned char byte4 __attribute__((vector_size(4)));
+typedef unsigned char byte5 __attribute__((vector_size(8)));  // Including padding
+typedef unsigned char byte6 __attribute__((vector_size(8)));  // Including padding
+typedef unsigned char byte7 __attribute__((vector_size(8)));  // Including padding
+typedef unsigned char byte8 __attribute__((vector_size(8)));
 
 typedef int int2 __attribute__((vector_size(8)));
-typedef int int3 __attribute__((vector_size(16))); // Including padding
+typedef int int3 __attribute__((vector_size(16)));  // Including padding
 typedef int int4 __attribute__((vector_size(16)));
+typedef int int5 __attribute__((vector_size(32)));  // Including padding
+typedef int int6 __attribute__((vector_size(32)));  // Including padding
+typedef int int7 __attribute__((vector_size(32)));  // Including padding
+typedef int int8 __attribute__((vector_size(32)));
 
 typedef float float2 __attribute__((vector_size(8)));
-typedef float float3 __attribute__((vector_size(16))); // Including padding
+typedef float float3 __attribute__((vector_size(16)));  // Including padding
 typedef float float4 __attribute__((vector_size(16)));
+typedef float float5 __attribute__((vector_size(32)));  // Including padding
+typedef float float6 __attribute__((vector_size(32)));  // Including padding
+typedef float float7 __attribute__((vector_size(32)));  // Including padding
+typedef float float8 __attribute__((vector_size(32)));
 
 typedef double double2 __attribute__((vector_size(16)));
-typedef double double3 __attribute__((vector_size(32))); // Including padding
+typedef double double3 __attribute__((vector_size(32)));  // Including padding
 typedef double double4 __attribute__((vector_size(32)));
+typedef double double5 __attribute__((vector_size(64)));  // Including padding
+typedef double double6 __attribute__((vector_size(64)));  // Including padding
+typedef double double7 __attribute__((vector_size(64)));  // Including padding
+typedef double double8 __attribute__((vector_size(64)));
 
 #elif defined(__cplusplus)
 
--- cvec234/vector_template.hpp
+++ cvec234/vector_template.hpp
@@ -47,15 +47,31 @@ struct VecTemp {
 typedef VecTemp<unsigned char, 2> byte2;
 typedef VecTemp<unsigned char, 3> byte3;
 typedef VecTemp<unsigned char, 4> byte4;
+typedef VecTemp<unsigned char, 5> byte5;
+typedef VecTemp<unsigned char, 6> byte6;
+typedef VecTemp<unsigned char, 7> byte7;
+typedef VecTemp<unsigned char, 8> byte8;
 
 typedef VecTemp<int, 2> int2;
 typedef VecTemp<int, 3> int3;
 typedef VecTemp<int, 4> int4;
+typedef VecTemp<int, 5> int5;
+typedef VecTemp<int, 6> int6;
+typedef VecTemp<int, 7> int7;
+typedef VecTemp<int, 8> int8;
 
 typedef VecTemp<float, 2> float2;
 typedef VecTemp<float, 3> float3;
 typedef VecTemp<float, 4> float4;
+typedef VecTemp<float, 5> float5;
+typedef VecTemp<float, 6> float6;
+typedef VecTemp<float, 7> float7;
+typedef VecTemp<float, 8> float8;
 
 typedef VecTemp<double, 2> double2;
 typedef VecTemp<double, 3> double3;
 typedef VecTemp<double, 4> double4;
+typedef VecTemp<double, 5> double5;
+typedef VecTemp<double, 6> double6;
+typedef VecTemp<double, 7> double7;
+typedef VecTemp<double, 8> double8;
