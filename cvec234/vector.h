4885dec59a94965cc570f36d274f429806d2ede5
Magnus Burenius <magnus.burenius@gmail.com>
2026-08-23 18:19:08 +0200

    handle expressions in NORMALIZED2 & 3

--- cvec234/vector.h
+++ cvec234/vector.h
@@ -26,8 +26,8 @@
 #define NORM2(a) (my_generic_sqrt(SQUARED_NORM2(a)))
 #define NORM3(a) (my_generic_sqrt(SQUARED_NORM3(a)))
 
-#define NORMALIZED2(a) (a / NORM2(a))
-#define NORMALIZED3(a) (a / NORM3(a))
+#define NORMALIZED2(a) ((a) / NORM2(a))
+#define NORMALIZED3(a) ((a) / NORM3(a))
 
 #define SQUARED_DISTANCE2(a, b) SQUARED_NORM2((a) - (b))
 #define SQUARED_DISTANCE3(a, b) SQUARED_NORM3((a) - (b))
